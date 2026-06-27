23b3dc987e7825274fc2cea27415fb28a7c6f97a
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-06-27 08:41:37 +0530

    Swagger Tag Updated

--- src/main/java/com/example/demo/controller/DatabaseController.java
+++ src/main/java/com/example/demo/controller/DatabaseController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.DatabaseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 // import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin")
+@Tag(name = "Truncate")
 public class DatabaseController {
 
     private final DatabaseService databaseService;
@@ -17,6 +20,7 @@ public class DatabaseController {
     }
 
     // @Hidden
+    @Operation(summary = "All Data")
     @DeleteMapping("/truncate")
     public String truncateDatabase() {
         databaseService.truncateAllTables();
