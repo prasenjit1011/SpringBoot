51ac81325902c7cf97fb8e7e5d5a9c01429a994e
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-06-29 07:13:43 +0530

    Food Listing

---
+++ src/main/java/com/example/demo/entity/Food.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "foods")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Food {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private Double price;
+}
